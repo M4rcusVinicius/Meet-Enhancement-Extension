d81a47b4b072e4bc30a241d403ff7023669b4885
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-06-01 13:36:17 -0300

    Import

--- background/constructor.js
+++ background/constructor.js
@@ -1,8 +1,19 @@
 const constructor = () => {
-  console.log('Constructor')
-  const iFrame  = document.createElement ("iframe");
-  iFrame.src = browser.extension.getURL('dashboard/home.html');
-  document.body.insertBefore(iFrame, document.body.firstChild);
-}
 
-constructor()
+  const xml = new XMLHttpRequest();
+  xml.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var div = document.createElement("div");
+      div.innerHTML = this.responseText;
+      document.body.insertBefore(div, document.body.firstChild);
+    } else {
+      console.log("files not found");
+    }
+  };
+
+  xml.open("GET", browser.extension.getURL("resources/dashboard.html"), true);
+  xml.send();
+};
+
+
+constructor();
