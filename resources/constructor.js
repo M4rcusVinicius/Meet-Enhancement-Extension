dbfc9b867cd62efa831b2545269cbce05f8a23aa
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-06-02 22:03:17 -0300

    Open and close

--- resources/constructor.js
+++ resources/constructor.js
@@ -3,13 +3,26 @@ function listener() {
     const button = document.querySelector('.ov7jof > span:nth-child(1) > button:nth-child(1)')
     console.log('Button', button)
     button.addEventListener('click', () => { open() })
+
+    const info = document.querySelector('div.r6xAKc:nth-child(1) > span:nth-child(1) > button:nth-child(1)')
+    info.addEventListener('click', () => { close() })
+    const users = document.querySelector('.SGP0hd > div:nth-child(2) > span:nth-child(1) > button:nth-child(1)')
+    users.addEventListener('click', () => { close() })
+    const chat = document.querySelector('div.r6xAKc:nth-child(3) > span:nth-child(1) > button:nth-child(1)')
+    chat.addEventListener('click', () => { close() })
   } catch(error) {
     console.log('Error on constructor')
   }
 }
 
 function open() {
+  console.log('Open')
   document.getElementById('dash').style.display = 'block'
+}
+
+function close() {
+  console.log('Close')
+  document.getElementById('dash').style.display = 'none'
 }
 
 function constructor() {
