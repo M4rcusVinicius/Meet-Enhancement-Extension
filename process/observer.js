40bdee56da480e25e683c25c7f4b9f4153dc6e4e
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-06-04 22:03:21 -0300

    Observer

--- process/observer.js
+++ process/observer.js
@@ -1,23 +1,18 @@
 function observer() {
   try {
-    const previous = () => {
-      const prev = localStorage.getItem('previous')
-      console.log('Previous:', prev)
-      if(!prev) { return {} }
-      return prev
-    }
     const users = getUsers()
-    const [result, change] = process(users, previous);
+    const [result, change] = process(users);
     localStorage.setItem('previous', result)
     console.log('Observer changes:', change)
     console.log('Observer result:', result)
+    db.previous = result
   } catch(error) {
     setError('Error on observer', error, true)
   }
 }
 
 function getUsers() {
-  const query = document.querySelectorAll('.GvcuGe')
+  const query = userQuery()
   const names = new Array
   return Object.values(query).map(user => {
     let name = user.querySelector(".ZjFb7c").innerText;
@@ -32,25 +27,41 @@ function getUsers() {
   });
 }
 
-function process(users, previous) {
+function userQuery() {
+  const query = document.querySelectorAll('.GvcuGe')
+  if ( query.length > 0 ) { return query }
+  console.log('>> Open user section to try to load users')
+
+  document.querySelector('.SGP0hd > div:nth-child(2) > span:nth-child(1) > button:nth-child(1)').click()
+  setTimeout(() => {
+  	document.querySelector('.ov7jof > span:nth-child(1) > button:nth-child(1)').click()
+    const query = document.querySelectorAll('.GvcuGe')
+    if (query.length = 0) { throw new Error('User query return 0 users') }
+    return query
+  }, 1000)
+}
+
+
+function process(users) {
   const result = new Object();
   const change = new Array();
 
   users.forEach(user => {
     const id = user.name.replace(/[^\w\s]/gi, "").replace(/\s/g, "-");
-    if (!(id in previous)) {
+    if (!(id in db.previous)) {
       { !user.muted && user.events.push("unmuted"); }
       user.events.push("new");
-    } else if (previous[id].muted !== user.muted) {
+    } else if (db.previous[id].muted !== user.muted) {
       if (user.muted) { user.events.push("muted"); }
       else { user.events.push("unmuted"); }
     }
-    delete previous[id];
+    delete db.previous[id];
     if (user.events.length > 0) { change.push(user); }
     result[id] = user;
   });
 
-  for (const [key, user] of Object.entries(previous)) {
+  for (const [key, user] of Object.entries(db.previous)) {
+    
     user.events.push("leave");
     change.push(user);
     result[key] = user;
