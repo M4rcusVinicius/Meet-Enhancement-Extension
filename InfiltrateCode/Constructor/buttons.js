8e2cfd12dc5a3a07a39b5bcec95d7250366f4739
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-06-11 16:11:07 -0300

    Mute function

--- InfiltrateCode/Constructor/buttons.js
+++ InfiltrateCode/Constructor/buttons.js
@@ -6,6 +6,7 @@ function buttons() {
       ['.SGP0hd > div:nth-child(2) > span:nth-child(1) > button:nth-child(1)', open],
       ['div.r6xAKc:nth-child(3) > span:nth-child(1) > button:nth-child(1)', close],
       ['#start', start],
+      ['#mute', mute],
     ]
     listener.forEach(button => {
     	document.querySelector(button[0]).addEventListener('click', button[1])
@@ -29,11 +30,37 @@ function close() {
 
 // ========================================================================================== //
 
+function mute() {
+  try {
+    const button = document.getElementById('mute')
+    button.removeEventListener('click', mute)
+    button.innerHTML = '<div class="VfPpkd-Jh9lGc"></div><div class="VfPpkd-RLmnJb"></div><i class="google-material-icons VfPpkd-kBDsod" aria-hidden="true">volume_up</i><span class="VfPpkd-vQzf8d">Ativar som</span>'
+    button.id = 'unmute'
+    button.addEventListener('click', unmute)
+    Object.values(document.querySelectorAll('audio')).map(audio => {
+      audio.muted = true
+    })
+  } catch(error) { setError('Error on mute button', error, true) }
+}
+
+function unmute() {
+  try {
+    const button = document.getElementById('unmute')
+    button.removeEventListener('click', unmute)
+    button.innerHTML = '<div class="VfPpkd-Jh9lGc"></div><div class="VfPpkd-RLmnJb"></div><i class="google-material-icons VfPpkd-kBDsod" aria-hidden="true">volume_off</i><span class="VfPpkd-vQzf8d">Desativar som</span>'
+    button.id = 'mute'
+    button.addEventListener('click', mute)
+    Object.values(document.querySelectorAll('audio')).map(audio => {
+      audio.muted = true
+    })
+  } catch(error) { setError('Error on start button', error, true) }
+}
+
 function start() {
   try {
     const button = document.getElementById('start')
     button.removeEventListener('click', start)
-    button.innerHTML = '<div class="VfPpkd-Jh9lGc"></div><div class="VfPpkd-RLmnJb"></div><i class="google-material-icons VfPpkd-kBDsod" aria-hidden="true">stop</i><span class="VfPpkd-vQzf8d">Parar</span>'
+    button.innerHTML = '< class="VfPpkd-Jh9lGc"></><div class="VfPpkd-RLmnJb"></div><i class="google-material-icons VfPpkd-kBDsod" aria-hidden="true">stop</i><span class="VfPpkd-vQzf8d">Parar</span>'
     button.id = 'stop'
     button.addEventListener('click', stop)
     db.play = true
