a1da58517a08de164936965c01d4b74438c0e657
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-06-14 11:29:28 -0300

    XML

--- Assets/injector.js
+++ Assets/injector.js
@@ -4,6 +4,7 @@
   URL: HTML relative path ( browser.extension.getURL() )
   ID: html id
   HTML: DOM element object
+  Place: Query for parent element
   Style: {
     src: style relative path
     id: style ID
@@ -12,30 +13,31 @@
 /" =========================================================== */
 
 
-function injector(message, url, id, html, style) {
+function injector(message, url, id, html, place, style, callback) {
   try {
-
+    injectHTML(message, url, id, html, place, callback)
   } catch(err) { }
 }
 
-function injectHTML(message, url, id, html) {
+function injectHTML(message, url, id, html, place, callback) {
   const xml = new XMLHttpRequest();
   xml.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      element.html.innerHTML = this.responseText;
-      document.querySelector(element.place).appendChild(element.html)
+      html.innerHTML = this.responseText;
+      document.querySelector(place).appendChild(html)
+      console.log()
     }
   };
-  xml.open("GET", element.url, true);
+  xml.open("GET", url, true);
   xml.send();
   xml.onerror = err => { 
     error('Error on create XML', [
-      ['Message: ', element.message],
-      ['Path: ', element.url],
-      ['ID: ', element.id],
-      ['HTML: ', element.id],
-      ['Style: ', element.id],
-    ], error )
+      ['Message: ', message],
+      ['Path: ', url],
+      ['ID: ', id],
+      ['HTML: ', html],
+      ['Place: ', place],
+    ], err )
   }
   xml.onloadend = callback
 }
