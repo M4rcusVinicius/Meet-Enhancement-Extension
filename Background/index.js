bcb4f6f6ea232e35204fd349257aeb613fc6fe37
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-06-14 15:01:42 -0300

    Remove prev elements

--- Background/index.js
+++ Background/index.js
@@ -1,3 +1,15 @@
+const prevComponents = [
+  '#initButton',
+  '#initCSS'
+]
+
+prevComponents.forEach(query => {
+  document.querySelectorAll(query).forEach(element => {
+    element.remove()
+  })
+})
+
+
 
 // Create button to implement all functions
 initButton()
